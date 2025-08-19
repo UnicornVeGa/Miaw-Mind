00b8bc18c54ca2c634038dd8d38637a258239e49
cinthyavg <cinthya.velezg@gmail.com>
2025-08-19 16:10:06 -0600

    Initial Commit

---
+++ Miaw Mind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Miaw Mind
+//
+//  Created by Cinthya Vélez Grande on 19/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
